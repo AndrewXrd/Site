760d6483413fd0a40c3ba94a512595dda3447bfa
Anton Ignacious Andrew <andrewkavin30@gmail.com>
2025-12-30 14:43:24 +0530

    glitch effect

--- src/pages/TitlePage.jsx
+++ src/pages/TitlePage.jsx
@@ -9,7 +9,7 @@ function TitlePage() {
         <div className="main">
             <img src={bgimg} className='backimg' />
             <div className="title">A N D R E W</div>
-            <img src={me} className='me' />
+                <img src={me} className="me" />
             <div className='roles'>
                 <p className='role1'>UI/UX Designer</p>
                 <p className='role2'>Front-End Developer</p>
